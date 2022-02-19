9b38dcdaa620662e0db6815986dada6ff84ef758
Ryan <batker@oregonstate.edu>
2022-02-19 15:53:10 -0800

    Arm 1

--- ArmPi/Camera.py
+++ ArmPi/Camera.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python3
 # encoding:utf-8
 import sys
-sys.path.append('/home/pi/ArmPi/')
+# sys.path.append('/home/pi/ArmPi/')
+sys.path.append('/home/pi/School/RobotSystems/ArmPi/')
 import cv2
 import time
 import threading
