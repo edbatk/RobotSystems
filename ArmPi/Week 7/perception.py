bcde56e10b7317e3ce61edefbd1f360900e5faf5
Ryan <batker@oregonstate.edu>
2022-02-19 17:38:41 -0800

    Arm 1

--- ArmPi/Week 7/perception.py
+++ ArmPi/Week 7/perception.py
@@ -92,9 +92,9 @@ class Perception():
             self.world_x, self.world_y = convertCoordinate(img_centerx, img_centery, self.size) # Convert to real world coordinates
             
             
-            cv2.drawContours(img, [box], -1, range_rgb[detect_color], 2)
+            cv2.drawContours(img, [box], -1, self.range_rgb[detect_color], 2)
             cv2.putText(img, '(' + str(self.world_x) + ',' + str(self.world_y) + ')', (min(box[0, 0], box[2, 0]), box[2, 1] - 10),
-                    cv2.FONT_HERSHEY_SIMPLEX, 0.5, range_rgb[detect_color], 1) # draw center point
+                    cv2.FONT_HERSHEY_SIMPLEX, 0.5, self.range_rgb[detect_color], 1) # draw center point
             distance = math.sqrt(pow(self.world_x - self.last_x, 2) + pow(self.world_y - self.last_y, 2)) # Compare the last coordinates to determine whether to move
             self.last_x, self.last_y = self.world_x, self.world_y
             
