3689bcaaf3f518f0762e6357d5afd4a8a01f4636
Ryan <batker@oregonstate.edu>
2022-02-24 16:48:27 -0800

    Week 8

---
+++ ArmPi/Week 8/move.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+# coding=utf8
+import sys
+sys.path.append('/home/pi/School/RobotSystems/ArmPi/')
+import cv2
+import time
+import Camera
+from math import sqrt
+from LABConfig import *
+from ArmIK.Transform import *
+from ArmIK.ArmMoveIK import *
+import HiwonderSDK.Board as Board
+from CameraCalibration.CalibrationConfig import *
+
+def initMove():
+    Board.setBusServoPulse(1, servo1 - 50, 300)
+    Board.setBusServoPulse(2, 500, 500)
+    AK.setPitchRangeMoving((0, 10, 10), -30, -30, -90, 1500)
+    
+if __name__ == "__main__":
+    initMove()
+
